42492521f8a40cb3156cde75ed657f01295897d3
Muhammad <89138415+Grom013@users.noreply.github.com>
2023-07-29 16:45:05 +0300

    add jest, add first test

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     env: {
         browser: true,
         es2021: true,
+        jest: true,
     },
     extends: [
         'plugin:react/recommended',
@@ -39,6 +40,7 @@ module.exports = {
         'no-unused-vars': 'off',
         'no-underscore-dangle': 'off',
         'i18next/no-literal-string': ['error', { markupOnly: true }],
+        'max-len': 'off',
     },
     globals: {
         __IS_DEV__: true,
