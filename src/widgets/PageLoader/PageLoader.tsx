d58e4be1e36f74980d981a219a405539da663dde
Muhammad <89138415+Grom013@users.noreply.github.com>
2023-07-29 22:18:02 +0300

    add page loader, add not found page, add error boundary

---
+++ src/widgets/PageLoader/PageLoader.tsx
@@ -0,0 +1,20 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { Loader } from 'shared/ui/Loader/Loader';
+import cls from './PageLoader.module.scss';
+
+interface PageLoaderProps{
+    className?:string
+}
+
+export const PageLoader = (props:PageLoaderProps) => {
+    const {
+        className,
+    } = props;
+    const { t } = useTranslation();
+    return (
+        <div className={classNames(cls.PageLoader, {}, [className])}>
+            <Loader />
+        </div>
+    );
+};
