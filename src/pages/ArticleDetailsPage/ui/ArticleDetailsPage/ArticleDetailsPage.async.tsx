8c0f3eced6b7f84511fa92107d10621d78742594
Muhammad <89138415+Grom013@users.noreply.github.com>
2023-08-16 19:10:31 +0300

    add fetching articles, view type selector

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.async.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.async.tsx
@@ -2,5 +2,5 @@ import { lazy } from 'react';
 
 export const ArticleDetailsPageAsync = lazy(() => new Promise((resolve) => {
     // @ts-ignore
-    setTimeout(() => resolve(import('./ArticleDetailsPage')));
+    setTimeout(() => resolve(import('./ArticleDetailsPage')), 400);
 }));
