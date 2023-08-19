0724667a32115b3d540706790ea2bc6a2626e0f7
Muhammad <89138415+Grom013@users.noreply.github.com>
2023-08-19 20:59:48 +0300

    add headless ui, add listbox component

--- src/entities/Currency/ui/CurrencySelect/CurrencySelect.tsx
+++ src/entities/Currency/ui/CurrencySelect/CurrencySelect.tsx
@@ -1,41 +1,42 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { Select } from 'shared/ui/Select/Select';
-import { Currency } from '../../model/types/currency';
 import { memo, useCallback } from 'react';
+import { ListBox, ListBoxItem } from 'shared/ui/ListBox/ListBox';
+import { Currency } from '../../model/types/currency';
 
-interface CurrencySelectProps{
-    className?:string;
+interface CurrencySelectProps {
+    className?: string;
     value?: Currency;
-    onChange?: (value: Currency)=> void;
+    onChange?: (value: Currency) => void;
     readonly?: boolean;
 }
 
-export const CurrencySelect = memo((props:CurrencySelectProps) => {
-    const {
-        className,
-        value,
-        onChange,
-        readonly,
-    } = props;
-    const options = [
-        { value: Currency.RUB, content: Currency.RUB },
-        { value: Currency.EUR, content: Currency.EUR },
-        { value: Currency.USD, content: Currency.USD },
-    ];
+const options = [
+    { value: Currency.RUB, content: Currency.RUB },
+    { value: Currency.EUR, content: Currency.EUR },
+    { value: Currency.USD, content: Currency.USD },
+];
+
+export const CurrencySelect = memo(({
+    className, value, onChange, readonly,
+}: CurrencySelectProps) => {
     const { t } = useTranslation();
+
     const onChangeHandler = useCallback((value: string) => {
         onChange?.(value as Currency);
     }, [onChange]);
+
     return (
-        <Select
-            className={classNames('', {}, [className])}
-            label={t('Укажите валюту')}
-            options={options}
+        <ListBox
+            className={className}
             value={value}
+            defaultValue={t('Укажите валюту')}
+            label={t('Укажите валюту')}
+            items={options}
             onChange={onChangeHandler}
             readonly={readonly}
+            direction="top"
         />
-
     );
 });
