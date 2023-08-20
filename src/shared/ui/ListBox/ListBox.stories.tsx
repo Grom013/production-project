46e96f30c20578f7e40dde89263200657ffaab12
Muhammad <89138415+Grom013@users.noreply.github.com>
2023-08-20 15:08:50 +0300

    add dropdown

--- src/shared/ui/ListBox/ListBox.stories.tsx
+++ src/shared/ui/ListBox/ListBox.stories.tsx
@@ -9,11 +9,57 @@ export default {
     argTypes: {
         backgroundColor: { control: 'color' },
     },
+    decorators: [
+        (Story) => <div style={{ padding: 100 }}><Story /></div>,
+    ],
 } as ComponentMeta<typeof ListBox>;
 
 const Template: ComponentStory<typeof ListBox> = (args) => <ListBox {...args} />;
 
 export const Normal = Template.bind({});
 Normal.args = {
+    items: [
+        { content: '1yfjhg34', value: '123' },
+        { content: '1yfjhg34', value: '123' },
+    ],
+};
 
+export const TopLeft = Template.bind({});
+TopLeft.args = {
+    direction: 'top left',
+    value: '123',
+    items: [
+        { content: '1yfjhg34', value: '123' },
+        { content: '1yfjhg34', value: '123' },
+    ],
+};
+
+export const TopRight = Template.bind({});
+TopRight.args = {
+    direction: 'top right',
+    value: '123',
+    items: [
+        { content: '1yfjhg34', value: '123' },
+        { content: '1yfjhg34', value: '123' },
+    ],
+};
+
+export const BottomLeft = Template.bind({});
+BottomLeft.args = {
+    direction: 'bottom left',
+    value: '123',
+    items: [
+        { content: '1yfjhg34', value: '123' },
+        { content: '1yfjhg34', value: '123' },
+    ],
+};
+
+export const BottomRight = Template.bind({});
+BottomRight.args = {
+    direction: 'bottom right',
+    value: '123',
+    items: [
+        { content: '1yfjhg34', value: '123' },
+        { content: '1yfjhg34', value: '123' },
+    ],
 };
